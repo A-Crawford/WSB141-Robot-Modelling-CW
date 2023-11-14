da121c8d98639fc092276ef8ee7313d8b2aeff02
Aidan <aidan.crawford@hotmail.com>
2023-11-14 17:37:52 +0000

    FK Solution, compelted DH transform matricies

--- RPPRRR_Manipulator_RTB.py
+++ RPPRRR_Manipulator_RTB.py
@@ -1,5 +1,6 @@
 import roboticstoolbox as rtb
 import numpy as np
+import RPPRRR_Manipulator_Numpy
 
 
 # Robotics Toolbox Solution
@@ -22,6 +23,7 @@ class RPPRRRManipulator():
 
         self.DH_Table = self.create_DH_table()
         self.model = self.create_RTB_robot()
+        T0_2 = self.Base_Tool_Transforms()
 
     def create_DH_table(self):
         # Default DH table is we use is Ln, Alpha, d, offset
@@ -29,12 +31,14 @@ class RPPRRRManipulator():
         # Will be using Robotics Toolbox conventiion
         # Could craete instance of rbt.serialLink directly but this allows for adjustment of table indepenetly
         dh_table = [
+            [0, 0, self.L0, 0],
             [0, 0, 0, self.theta1],
             [0, 0, self.d2, 0],
-            [0, 90, self.d3, 0],
+            [0, np.radians(90), self.d3, 0],
             [0, 0, self.L1, self.theta4],
-            [self.L2, 90, self.L5, self.theta5],
+            [self.L2, np.radians(90), self.L5, self.theta5],
             [0, 0, self.L3, self.theta6],
+            [0, 0, self.L4, 0]
         ]
         return dh_table
     
@@ -42,18 +46,34 @@ class RPPRRRManipulator():
         # R P P R R R 
         robot = rtb.DHRobot(
             [
-                rtb.RevoluteDH(d=self.DH_Table[0][0], a=self.DH_Table[0][1], alpha=self.DH_Table[0][2], offset=self.DH_Table[0][3]),
-                rtb.PrismaticDH(q=self.DH_Table[1][0], a=self.DH_Table[1][1], alpha=self.DH_Table[1][2], offset=self.DH_Table[1][3]),
-                rtb.PrismaticDH(q=self.DH_Table[2][0], a=self.DH_Table[2][1], alpha=self.DH_Table[2][2], offset=self.DH_Table[2][3]),
-                rtb.RevoluteDH(d=self.DH_Table[3][0], a=self.DH_Table[3][1], alpha=self.DH_Table[3][2], offset=self.DH_Table[3][3]),
-                rtb.RevoluteDH(d=self.DH_Table[4][0], a=self.DH_Table[4][1], alpha=self.DH_Table[4][2], offset=self.DH_Table[4][3]),
-                rtb.RevoluteDH(d=self.DH_Table[5][0], a=self.DH_Table[5][1], alpha=self.DH_Table[5][2], offset=self.DH_Table[5][3]),
+                rtb.PrismaticDH(q=self.DH_Table[0][2]), # Base - Added for matricies, not an actual joint to be used
+
+                rtb.RevoluteDH(a=self.DH_Table[1][0], alpha=self.DH_Table[1][1], d=self.DH_Table[1][2], offset=self.DH_Table[1][3]),
+
+                rtb.PrismaticDH(a=self.DH_Table[2][0], alpha=self.DH_Table[2][1], q=self.DH_Table[2][2], offset=self.DH_Table[2][3]),
+                rtb.PrismaticDH(a=self.DH_Table[3][0], alpha=self.DH_Table[3][1], q=self.DH_Table[3][2], offset=self.DH_Table[3][3]),
+
+                rtb.RevoluteDH(a=self.DH_Table[4][0], alpha=self.DH_Table[4][1], d=self.DH_Table[4][2], offset=self.DH_Table[4][3]),
+                rtb.RevoluteDH(a=self.DH_Table[5][0], alpha=self.DH_Table[5][1], d=self.DH_Table[5][2], offset=self.DH_Table[5][3]),
+                rtb.RevoluteDH(a=self.DH_Table[6][0], alpha=self.DH_Table[6][1], d=self.DH_Table[6][2], offset=self.DH_Table[6][3]),
+
+                rtb.PrismaticDH(q=self.DH_Table[7][2]) # Tool - Not to be used as an actual joint
             ],
-            base=[0, 0, 0, 0],
             name="RPPRRR Manipulator"
         )
-        print(robot)
         return robot
+    
+
+    def Base_Tool_Transforms(self):
+
+        Trasnforms = RPPRRR_Manipulator_Numpy.RPPRRRManipulator()
+
+        T0_2 = Trasnforms.TB_1.dot(Trasnforms.T1_2)
+        print(T0_2)
+        return T0_2
+
+
+
     
 
     
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import RPPRRR_Manipulator_RTB 
-import RPPRRR_Manipulator_Numpy
 import roboticstoolbox as rtb
+import matplotlib as plt
 import numpy as np
 
 if __name__ == "__main__":
     RBTmanipulator = RPPRRR_Manipulator_RTB.RPPRRRManipulator().model
-    NumpyManipulator = RPPRRR_Manipulator_Numpy.RPPRRRManipulator()
 
-    Transformation_Matrix = RBTmanipulator.fkine([0, 0.5, 0, 0, 0, 0])
+    print(RBTmanipulator)
+
+    FKTransform = RBTmanipulator.fkine([0, 0, 0.5, 0, 0, 0, 0, 0]) #Base=0, 1, 2, 3, 4, 5, 6, Tool=0 
+    print("Forward Kinematics with joint angles [0, 0.5, 0, 0, 0]: \n", FKTransform)
+
     RBTmanipulator.plot(RBTmanipulator.q)
-
-    print('Numpy Ans: \n', NumpyManipulator.TBase_Tool)
-
-    NumpyManipulator.plot_frames()
 
     input('Press any button to continue...')
