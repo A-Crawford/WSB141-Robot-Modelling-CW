3037a54e9eb6e1970e4b71904c66f84f82f95401
Aidan <aidan.crawford@hotmail.com>
2023-11-20 19:47:44 +0000

    defined class for rpprrr which inherits from DHRobot, defined orward kinematic method

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import roboticstoolbox as rtb
 import matplotlib as plt
 import numpy as np
 
+from rpprrr_manipulator import RPPRRRManipulator
+
 IK_Transform_1 = np.array(
     [
         [1, 0, 0, -0.3],
@@ -31,30 +33,35 @@ IK_Transform_3 = np.array(
 )
 
 if __name__ == "__main__":
-    RBTManipulator = RPPRRR_Manipulator_RTB.RPPRRRManipulator().model
 
-    print(RBTManipulator)
-    print(RBTManipulator.q)
-
-    FK_Transform = RBTManipulator.fkine([0, 0, 0.5, 0, 0, 0, 0, 0])
-    print('Forward Kinematics: \n', FK_Transform)
-
-    IK_Sol = RBTManipulator.ikine_LM(FK_Transform)
-    print('Inverse Kinematics of Foraward Kinematics: \n', IK_Sol)
-
-    IK_1_Sol = RBTManipulator.ikine_LM(IK_Transform_1)
-    print(IK_1_Sol)
-
-    IK_2_Sol = RBTManipulator.ikine_LM(IK_Transform_2)
-    print(IK_2_Sol)
-
-    IK_3_Sol = RBTManipulator.ikine_LM(IK_Transform_3)
-    print(IK_3_Sol)
+    manipulator = RPPRRRManipulator()
+    manipualtor_fk = manipulator.forward_kinematics([0, 0.5, 0, 0, 0, 0])
 
 
-    print(np.round(np.degrees(IK_Sol.q[:])))
+    # RBTManipulator = RPPRRR_Manipulator_RTB.RPPRRRManipulator().model
 
-    print('Error: ', np.linalg.norm(FK_Transform - RBTManipulator.fkine(IK_Sol.q)))
+    # print(RBTManipulator)
+    # print(RBTManipulator.q)
 
-    # RBTManipulator.plot([0, 0, 0.5, 0, 0, 0, 0, 0], block=True)
-    # RBTManipulator.plot(IK_1_Sol.q, block=True)
+    # FK_Transform = RBTManipulator.fkine([0, 0, 0.5, 0, 0, 0, 0, 0])
+    # print('Forward Kinematics: \n', FK_Transform)
+
+    # IK_Sol = RBTManipulator.ikine_LM(FK_Transform)
+    # print('Inverse Kinematics of Foraward Kinematics: \n', IK_Sol)
+
+    # IK_1_Sol = RBTManipulator.ikine_LM(IK_Transform_1)
+    # print(IK_1_Sol)
+
+    # IK_2_Sol = RBTManipulator.ikine_LM(IK_Transform_2)
+    # print(IK_2_Sol)
+
+    # IK_3_Sol = RBTManipulator.ikine_LM(IK_Transform_3)
+    # print(IK_3_Sol)
+
+
+    # print(np.round(np.degrees(IK_Sol.q[:])))
+
+    # print('Error: ', np.linalg.norm(FK_Transform - RBTManipulator.fkine(IK_Sol.q)))
+
+    # # RBTManipulator.plot([0, 0, 0.5, 0, 0, 0, 0, 0], block=True)
+    # # RBTManipulator.plot(IK_1_Sol.q, block=True)
---
+++ rpprrr_manipulator.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+"""
+@author: Aidan Crawford
+"""
+
+import numpy as np
+from roboticstoolbox import DHRobot, RevoluteDH, PrismaticDH
+from spatialmath import SE3
+
+class RPPRRRManipulator(DHRobot):
+    """
+    Class to model and interact with RPPRRR Manipulator as defined in 23WSB141 - Introduction To Robotics Coursework
+    """
+
+    def __init__(self):
+        # Define Parameters in DH Table and specified in FK 'actual dimensions'
+        self.L0 = 0.10
+        self.L1 = 0.20
+        self.L2 = 0.30
+        self.L3 = 0.30
+        self.L4 = 0.10
+        self.L5 = 0.05
+        self.THETA1 = 0
+        self.THETA4 = 0
+        self.THETA5 = 0
+        self.THETA6 = 0
+        self.D2 = 0.5
+        self.D3 = 0
+
+        self.DH_TABLE = np.array(
+            [
+                [0, 0, self.L0, 0],
+                [0, 0, 0, self.THETA1],
+                [0, 0, self.D2, 0],
+                [np.radians(90), 0, self.D3, 0],
+                [0, 0, self.L1, self.THETA4],
+                [np.radians(90), self.L2, self.L5, self.THETA6],
+                [0, 0, self.L3, self.THETA6],
+                [0, 0, self.L4, 0]
+            ]
+        )
+
+        links = [
+            RevoluteDH(# Fake joint to mimic base frame 
+                alpha=self.DH_TABLE[0][0], 
+                a=self.DH_TABLE[0][1], 
+                d=self.DH_TABLE[0][2], 
+                offset=self.DH_TABLE[0][3], 
+                qlim=np.array([0, 0])
+                ), 
+
+            RevoluteDH(
+                alpha=self.DH_TABLE[1][0], 
+                a=self.DH_TABLE[1][1], 
+                d=self.DH_TABLE[1][2], 
+                offset=self.DH_TABLE[1][3], 
+                qlim=np.array([np.radians(-180), np.radians(180)])
+                ), 
+
+            PrismaticDH(
+                alpha=self.DH_TABLE[2][0], 
+                a=self.DH_TABLE[2][1], 
+                q=self.DH_TABLE[2][2], 
+                offset=self.DH_TABLE[2][3], 
+                qlim=np.array([0.0, 0.5])
+                ),
+            PrismaticDH(
+                alpha=self.DH_TABLE[3][0], 
+                a=self.DH_TABLE[3][1], 
+                q=self.DH_TABLE[3][2], 
+                offset=self.DH_TABLE[3][3], 
+                qlim=np.array([-0.1, 0.1])
+                ),
+
+            RevoluteDH(
+                alpha=self.DH_TABLE[4][0],
+                a=self.DH_TABLE[4][1], 
+                d=self.DH_TABLE[4][2], 
+                offset=self.DH_TABLE[4][3], 
+                qlim=np.array([np.radians(-90), np.radians(90)])
+                ),
+            RevoluteDH(
+                alpha=self.DH_TABLE[5][0], 
+                a=self.DH_TABLE[5][1], 
+                d=self.DH_TABLE[5][2], 
+                offset=self.DH_TABLE[5][3], 
+                qlim=np.array([np.radians(-180), np.radians(180)])
+                ),
+            RevoluteDH(
+                alpha=self.DH_TABLE[6][0], 
+                a=self.DH_TABLE[6][1], 
+                d=self.DH_TABLE[6][2], 
+                offset=self.DH_TABLE[6][3], 
+                qlim=np.array([np.radians(-90), np.radians(90)])
+                ),
+
+            RevoluteDH( #Fake joint to mimic tool frame
+                alpha=self.DH_TABLE[7][0], 
+                a=self.DH_TABLE[7][1], 
+                d=self.DH_TABLE[7][2], 
+                offset=self.DH_TABLE[7][3], qlim=np.array([0, 0])
+                ) 
+        ]
+
+        super().__init__(
+            links, 
+            name="RPPRRR Manipulator"
+        )
+
+    def forward_kinematics(self, joint_angles: list) -> SE3:
+        '''
+        Calculates forward kinematics from five (5) joint angles
+
+        :param joint_angles: joint angles in radians
+        :type joint_angles: List
+
+        :return FK_Sol: Transformation matrix of end effectors forward kinematics
+        :type FK_sol: SE3
+
+        .. note::
+            - Angles are in radians
+            - Automatically adds a zero to the start and end of `joint_angles` to account for the placeholder joints used in the model to represent the base and end effector
+            - If a solution cannot be found None will be returned
+        '''
+        if type(joint_angles) is not list:
+            raise TypeError(f"{type(joint_angles)} is not valid. {list} expected.")
+
+        try:
+            if len(joint_angles) == 6:
+                joint_angles.insert(0, 0)
+                joint_angles.append(0)
+            else:
+                raise Exception("Incorrect array size. 6 joint angles are required")
+        except Exception as e:
+            print("An error occured: ", e)
+
+        try: 
+            fk_sol = self.fkine(joint_angles)
+        except Exception as e:
+            print("An error occured while calculating forawrd kinematics: ", e)
+            return False
+
+        return fk_sol
+    
+    def inverse_kinematics(self):
+        '''
+        Given a desired positon P and Orientation R, in the form of a SE3 transformation matrix, will return whether the manipulator can reach and the joint angles to do so.
+
+        :param transform: SE3 array
+        '''
+        
